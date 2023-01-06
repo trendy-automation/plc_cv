742b8afda7cee016655a62bab3b27ed10030991d
trendy-automation <ilya-kolesnic@yandex.ru>
2023-01-06 21:57:05 +0300

    try except while

--- python/plc.py
+++ python/plc.py
@@ -9,13 +9,13 @@ import time
 import threading
 from snap7.util import *
 
-#TODO found_part_num
-found_part_num = 0
-camera_DB_num = 8
-reconnect_timeout = 60
 
 class PLC(threading.Thread):
     def __init__(self, plc_ip):
+        # TODO found_part_num
+        found_part_num = 0
+        camera_DB_num = 8
+        reconnect_timeout = 60
         # init
         threading.Thread.__init__(self, args=(), name=plc_ip, kwargs=None)
         self.plc_ip = plc_ip
@@ -51,40 +51,44 @@ class PLC(threading.Thread):
         cur_thread = threading.current_thread()
         # Основной цикл
         while getattr(cur_thread, "do_run", True):
-            time.sleep(1.2)
-            if self.unreachable_time == 0 or (time.time() - self.unreachable_time) > reconnect_timeout:
-                if not self.snap7client.get_connected():
-                    # Подключение к контроллеру ...
-                    try:
-                        self.connection_ok = False
-                        self.logger.info(f"Подключение к контроллеру {self.plc_ip}...")
-                        self.snap7client.connect(self.plc_ip, 0, 1)
-                    except Exception as error:
-                        self.logger.error(f"Не удалось подключиться к контроллеру: {self.plc_ip}\n"
-                                          f"Ошибка {str(error)} {traceback.format_exc()}")
-                        snap7.client.logger.disabled = True
-                        self.unreachable_time = time.time()
-                else:
-                    if not self.connection_ok:
-                        self.connection_ok = True
-                        self.unreachable_time = 0
-                        self.logger.info(f"Соединение открыто {self.plc_ip}")
-                        snap7.client.logger.disabled = False
-                    try:
-                        snapshotReq = self.get_bool(db_number=camera_DB_num, offsetbyte=0, offsetbit=0)
-                    except Exception as error:
-                        self.logger.error(f"Не удалось считать строб съёмки: DB{camera_DB_num}.DBX0.0\n"
-                                          f"Ошибка {str(error)} {traceback.format_exc()}")
-                        self.snap7client.disconnect()
+            try:
+                time.sleep(1.2)
+                if self.unreachable_time == 0 or (time.time() - self.unreachable_time) > reconnect_timeout:
+                    if not self.snap7client.get_connected():
+                        # Подключение к контроллеру ...
+                        try:
+                            self.connection_ok = False
+                            self.logger.info(f"Подключение к контроллеру {self.plc_ip}...")
+                            self.snap7client.connect(self.plc_ip, 0, 1)
+                        except Exception as error:
+                            self.logger.error(f"Не удалось подключиться к контроллеру: {self.plc_ip}\n"
+                                              f"Ошибка {str(error)} {traceback.format_exc()}")
+                            snap7.client.logger.disabled = True
+                            self.unreachable_time = time.time()
+                    else:
+                        if not self.connection_ok:
+                            self.connection_ok = True
+                            self.unreachable_time = 0
+                            self.logger.info(f"Соединение открыто {self.plc_ip}")
+                            snap7.client.logger.disabled = False
+                        try:
+                            snapshotReq = self.get_bool(db_number=camera_DB_num, offsetbyte=0, offsetbit=0)
+                        except Exception as error:
+                            self.logger.error(f"Не удалось считать строб съёмки: DB{camera_DB_num}.DBX0.0\n"
+                                              f"Ошибка {str(error)} {traceback.format_exc()}")
+                            self.snap7client.disconnect()
 
 
-                    if snapshotReq:
-                        if found_part_num > 0:
-                           self.logger.info(f"Запись результата распознования - номер найденной детали - {found_part_num}")
-                           try:
-                               self.set_usint(db_number=camera_DB_num, offsetbyte=1, tag_value=found_part_num)
-                               found_part_num = 0
-                           except Exception as error:
-                               self.logger.error(f"Не удалось записать результат съёмки: DB{camera_DB_num}.DBB1\n"
-                                                 f"Ошибка {str(error)} {traceback.format_exc()}")
-                               self.snap7client.disconnect()
+                        if snapshotReq:
+                            if found_part_num > 0:
+                               self.logger.info(f"Запись результата распознования - номер найденной детали - {found_part_num}")
+                               try:
+                                   self.set_usint(db_number=camera_DB_num, offsetbyte=1, tag_value=found_part_num)
+                                   found_part_num = 0
+                               except Exception as error:
+                                   self.logger.error(f"Не удалось записать результат съёмки: DB{camera_DB_num}.DBB1\n"
+                                                     f"Ошибка {str(error)} {traceback.format_exc()}")
+                                   self.snap7client.disconnect()
+            except Exception as error:
+                self.logger.error(f"Не удалось обработать цикл класса plc\n"
+                                  f"Ошибка {str(error)} {traceback.format_exc()}")
