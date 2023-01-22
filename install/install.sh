1399990596d2bd368811efe0682e9f1bae77a152
trendy-automation <ilya-kolesnic@yandex.ru>
2023-01-22 13:47:35 +0300

    install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,16 +1,19 @@
-
 #!/bin/bash
-sudo apt install openssh-server
+sudo apt install -y openssh-server
 
 #insted https://docs.docker.com/engine/install/ubuntu/
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker $USER
+sudo apt  install -y docker-compose 
 
 git clone https://github.com/trendy-automation/plc_cv plc_cv
 
 cd plc_cv
-cp -ir python docker-compose/appdata
+cp -ir python docker-compose/appdata/python
 cd docker-compose
-docker-compose up --build -d
-docker-compose exec agent chown jenkins /var/run/docker.sock
+sudo chmod +x appdata/python/appstart.sh
+
+sudo docker-compose up --build -d
+sudo docker-compose exec agent chown jenkins /var/run/docker.sock
+sudo docker-compose exec jenkins cat var/jenkins_home/secrets/initialAdminPassword
